b98adfe0ca324df27149b27c4fed2b3f8441a133
MrTeleGrami <raminsalofficial@gmail.com>
2022-08-02 13:47:29 +0430

    Add files via upload

---
+++ Lib/Config.lua
@@ -0,0 +1,36 @@
+Config = {
+ 
+
+
+
+SUDO_ID = {935728510,1048853495,5456599625}, --ایدی سودو و ربات سی ال ای و جوینر و هلپر
+Full_Sudo = {935728510}, --ایدی مالک اصلی
+Sudoid = 935728510, --ایدی مالک اصلی ربات 
+BotCliId = 1048853495, --ایدی ربات سی ال ای     
+IDSup = 935728510, -- توسعه دهنده
+BotJoiner = 5456599625, --ایدی ربات
+JoinToken = '5456599625:AAGxPvtVcSbjbguFSvUa0jc1JTUGN7qGF1A', --توکن ربات جوینر
+UserHelper = '@AntiTele_Bot', --یوزرنیم ربات هلپر
+Paybot = 'AntiTele_Bot',
+LinkSuppoRt = 'https://t.me/joinchat/K_Tc799PB_VjMDdk', --لینک گروه پشتیبانی شما
+Channel = 'TeleBot_team', --ایدی چنل شما
+Sudo1 = 'MrTelegrami', --ایدی سودو       
+RedisIndex = 1, -- شمار رديس را در اينجا قرار دهيد (اگر نمیدونید چی هست تغییری ایجاد نکنید) --> base:Select ...-
+TD_ID = 1048853495, --شماره ردیس 
+ApiID = '1503047',
+ApiHash = '35df6ebd9a8cfc16f7742f9b8fd5dc01',
+SessionApi = 'APIBot',
+SessionCli = 'ClIBot',
+SessionTab = 'TabchiBot',
+UserBot = 'telebot',
+Leader = 935728510,
+NameBot = '【 Coded By TᴇʟᴇBᴏᴛ 】',
+
+ 
+
+
+----------------------------------------------
+} 
+
+ 
+return Config
