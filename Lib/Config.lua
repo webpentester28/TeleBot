65d99c91dd0455728986d4baac3d32689c1e72dd
webpentester28 <113394129+webpentester28@users.noreply.github.com>
2022-09-14 23:54:18 +0430

    Update Config.lua

--- Lib/Config.lua
+++ Lib/Config.lua
@@ -3,28 +3,28 @@ Config = {
 
 
 
-SUDO_ID = {935728510,1048853495,5456599625}, --ایدی سودو و ربات سی ال ای و جوینر و هلپر
-Full_Sudo = {935728510}, --ایدی مالک اصلی
-Sudoid = 935728510, --ایدی مالک اصلی ربات 
-BotCliId = 1048853495, --ایدی ربات سی ال ای     
-IDSup = 935728510, -- توسعه دهنده
-BotJoiner = 5456599625, --ایدی ربات
-JoinToken = '5456599625:AAGxPvtVcSbjbguFSvUa0jc1JTUGN7qGF1A', --توکن ربات جوینر
-UserHelper = '@AntiTele_Bot', --یوزرنیم ربات هلپر
+SUDO_ID = {1574130132,5395702735,5540413240}, --ایدی سودو و ربات سی ال ای و جوینر و هلپر
+Full_Sudo = {1574130132}, --ایدی مالک اصلی
+Sudoid = 1574130132, --ایدی مالک اصلی ربات 
+BotCliId = 5395702735, --ایدی ربات سی ال ای     
+IDSup = 1574130132, -- توسعه دهنده
+BotJoiner = 5540413240, --ایدی ربات
+JoinToken = '5540413240:AAEL_r2FK8cZAl-Ff701nwvgEqavdYze3jk', --توکن ربات جوینر
+UserHelper = '@helperTeleROBiXbot', --یوزرنیم ربات هلپر
 Paybot = 'AntiTele_Bot',
-LinkSuppoRt = 'https://t.me/joinchat/K_Tc799PB_VjMDdk', --لینک گروه پشتیبانی شما
-Channel = 'TeleBot_team', --ایدی چنل شما
-Sudo1 = 'MrTelegrami', --ایدی سودو       
+LinkSuppoRt = 'https://t.me/+9W3A_oAdZtk0NWM0', --لینک گروه پشتیبانی شما
+Channel = 'shror', --ایدی چنل شما
+Sudo1 = 'ARDIN_GHYAMT', --ایدی سودو       
 RedisIndex = 1, -- شمار رديس را در اينجا قرار دهيد (اگر نمیدونید چی هست تغییری ایجاد نکنید) --> base:Select ...-
 TD_ID = 1048853495, --شماره ردیس 
-ApiID = '1503047',
-ApiHash = '35df6ebd9a8cfc16f7742f9b8fd5dc01',
+ApiID = '13707724',
+ApiHash = 'f5819f7abba9995698a1e233d6702b7e',
 SessionApi = 'APIBot',
 SessionCli = 'ClIBot',
 SessionTab = 'TabchiBot',
 UserBot = 'telebot',
 Leader = 935728510,
-NameBot = '【 Coded By TᴇʟᴇBᴏᴛ 】',
+NameBot = '【 Coded By TeleROBiX 】',
 
  
 
